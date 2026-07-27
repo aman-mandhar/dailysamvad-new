0a5b2cfcb0cb29fcbd31fe0c42412a7b85af3962
aman-mandhar <parthmultisolution@gmail.com>
2026-07-27 21:12:18 +0530

    1

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html lang="@yield('language', str_replace('_', '-', app()->getLocale()))" class="scheme-light dark:scheme-dark">
     <head>
+        @if(config('services.google_analytics.measurement_id'))
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google_analytics.measurement_id') }}"></script>
+
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+
+        gtag('js', new Date());
+
+        gtag('config', '{{ config('services.google_analytics.measurement_id') }}');
+        </script>
+        @endif
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
