00714e399b2582f06353ecb83b4bce9e354ca7e0
aman-mandhar <parthmultisolution@gmail.com>
2026-07-27 19:22:07 +0530

    1

--- resources/views/components/frontend/logo.blade.php
+++ resources/views/components/frontend/logo.blade.php
@@ -6,7 +6,7 @@
     @if (is_file($logoPath))
         <img src="{{ asset('images/frontend/daily-samvad-logo.jpg') }}" alt="Daily Samvad" width="260" height="75">
     @else
-        <span class="ds-logo__fallback" aria-hidden="true">डेली संवाद</span>
+        <span class="ds-logo__fallback" aria-hidden="true">Daily संवाद</span>
         <span class="ds-logo__domain" aria-hidden="true">DAILYSAMVAD.COM</span>
     @endif
 </a>
