19dd75be6c0810f91248442717fba4877ff962b3
aman-mandhar <parthmultisolution@gmail.com>
2026-08-04 04:09:07 +0530

    1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,23 +16,9 @@
                     <x-frontend.section-heading id="video-news-heading">Video News</x-frontend.section-heading>
                     <div class="space-y-4">
                         <x-youtube-playlist-player placement="homepage" />
-                        <x-youtube-playlist-player placement="homepage-gallery" layout="grid" :limit="20" />
+                        <x-youtube-playlist-player placement="homepage-gallery" layout="grid" :limit="24" />
                     </div>
                 </section>
-
-                @if ($featuredPosts->isNotEmpty())
-                    <section aria-labelledby="featured-news-heading">
-                        <x-frontend.section-heading id="featured-news-heading">Featured News</x-frontend.section-heading>
-                        <div class="grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
-                            @foreach ($featuredPosts as $post)
-                                <x-news.medium-card :post="$post" />
-                            @endforeach
-                        </div>
-                    </section>
-                @endif
-
-                
-
             </div>
 
             <x-news.sidebar.index :widgets="$sidebarWidgets" :sticky="$sidebarSticky" />
